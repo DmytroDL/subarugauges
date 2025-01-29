e9870370aaaf19f9167c76e1c6da100e8128cdec
DmytroDL <103884409+DmytroDL@users.noreply.github.com>
2025-01-30 01:43:45 +0200

    Update bosch_temp.ino

--- bosch_temp.ino
+++ bosch_temp.ino
@@ -1,8 +1,3 @@
-#include <microDS18B20.h>
-#include <Arduino.h>
-#include <U8g2lib.h>
-#include <Wire.h>
-
 #define coolantsensorDivider 3307   //defines the resistor value that is in series in the voltage divider
 #define coolantsensorPin A0         //defines the analog pin of the input voltage from the voltage divider
 #define NUMSAMPLES 5                //defines the number of samples to be taken for a smooth average
@@ -12,14 +7,10 @@ const float steinconstB = 0.0002636218288;       //steinhart equation constant B
 const float steinconstC = 0.0000001401344732;    //steinhart equation constant C, determined from wikipedia equations
 int samples[NUMSAMPLES];                              //variable to store number of samples to be taken
 
-MicroDS18B20<2> sensor;
-U8G2_SSD1306_64X32_1F_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
-
 void setup() {
   Serial.begin(9600);
-  u8g2.begin();
-  u8g2.clearDisplay();
 }
+
 void loop() {
   uint8_t i;                                          //integer for loop
   float average;                                      //decimal for average
@@ -44,15 +35,5 @@ void loop() {
   steinhart += steinconstA;                     //Complete equation, 1/T=A+BlnR+C(lnR)^3
   steinhart = 1.0/steinhart;                    //Inverse to isolate for T
   steinhart -= 273.15;                          //Conversion from kelvin to celcius
-  //Serial.println("Temperature = "+ String(steinhart));
-  sensor.requestTemp();
-  delay(1000);
-  u8g2.clearDisplay();
-  Serial.println("DS18B20 Temperature = " + String(sensor.getTemp()) + ", BOSCH Temperature = " + String(steinhart));
-  u8g2.setFont(u8g2_font_t0_18_tf);  
-  u8g2.setCursor(2, 16);
-  u8g2.print(sensor.getTemp());
-  u8g2.setCursor(2, 32);
-  u8g2.print(steinhart);
-  u8g2.sendBuffer();
+  Serial.println("Temperature = "+ String(steinhart));
 }
