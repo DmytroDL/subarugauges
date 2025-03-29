0132f3dccef539941796cce25dcf091f7d4aa1b5
DmytroDL <103884409+DmytroDL@users.noreply.github.com>
2025-03-29 20:49:46 +0200

    Update EGT.ino

--- EGT.ino
+++ EGT.ino
@@ -1,8 +1,8 @@
 #include <GyverMAX6675.h>
 
-#define CLK_PIN   2  // Пін SCK
-#define DATA_PIN  4  // Пін SO
-#define CS_PIN    3  // Пін CS
+#define CLK_PIN   2  // SCK Pin
+#define DATA_PIN  4  // SO Pin
+#define CS_PIN    3  // CS Pin
 
 GyverMAX6675<CLK_PIN, DATA_PIN, CS_PIN> sens;
 
